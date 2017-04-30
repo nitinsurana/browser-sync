321d862c74fb4efd6b021b2bd05234ddd0623016
Nitin Surana <surana.nitin1990@gmail.com>
2017-04-30 09:02:04 -0700

    1. Fixed browser-sync to use local browser-sync-client & UI dependencies 2. Added ability to store events on firebase 3. Added todo.js as the main example file

---
+++ examples/todo.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var browserSync = require("../index").create();
+
+var config = {
+    proxy: "http://todomvc.com/examples/backbone/",
+    // proxy: 'https://www.w3schools.com/angular/tryit.asp?filename=try_ng_todo_app',
+    //files: ["app/css/*.css"]
+    minify: false
+};
+
+browserSync.init(config, function (err, bs) {
+    // Full access to Browsersync object here
+    console.log(bs.getOption("urls"));
+});
